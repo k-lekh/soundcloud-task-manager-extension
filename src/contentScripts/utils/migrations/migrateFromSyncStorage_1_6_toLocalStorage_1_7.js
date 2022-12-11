11520333663eaa54aedd7cf8dbf8679f86074519
futurfrukt <ks.lekh@gmail.com>
2022-12-11 14:14:01 +0100

    v1.7: move from sync to local storage

--- src/contentScripts/utils/migrations/migrateFromSyncStorage_1_6_toLocalStorage_1_7.js
+++ src/contentScripts/utils/migrations/migrateFromSyncStorage_1_6_toLocalStorage_1_7.js
@@ -3,12 +3,15 @@ import { log, error } from '../log';
 
 export async function migrateFromSyncStorage_1_6_toLocalStorage_1_7() {
   try {
-    const data = await new Promise((resolve) => chrome.storage.sync.get([STORAGE_TRACKS], (storageData = {}) => {
-      resolve(storageData);
+    const tracks = await new Promise((resolve) => chrome.storage.sync.get([STORAGE_TRACKS], (storageData = {}) => {
+      resolve(storageData[STORAGE_TRACKS]);
     }));
-    await chrome.storage.local.set(data);
-    await new Promise((resolve) => chrome.storage.sync.set({[STORAGE_TRACKS]:{}}, resolve));
-    log('>> migrateFromSyncStorage_1_6_toLocalStorage_1_7', 'done', data);
+    if (Object.keys(tracks).length) {
+      const data = { [STORAGE_TRACKS]: tracks };
+      await chrome.storage.local.set(data);
+      await new Promise((resolve) => chrome.storage.sync.set({ [STORAGE_TRACKS]: {} }, resolve));
+      log('>> migrateFromSyncStorage_1_6_toLocalStorage_1_7', 'done', data);
+    }
   } catch(e) {
     error('>> migrateFromSyncStorage_1_6_toLocalStorage_1_7', e);
   }
--- src/contentScripts/utils/storage.js
+++ src/contentScripts/utils/storage.js
@@ -2,7 +2,7 @@ import { STORAGE_TRACKS } from './constants';
 
 export async function storageGetTrack(trackId) {
   return new Promise((resolve) => {
-    chrome.storage.sync.get([STORAGE_TRACKS], (storageData = {}) => {
+    chrome.storage.local.get([STORAGE_TRACKS], (storageData = {}) => {
       const storageTracks = storageData[STORAGE_TRACKS] || {};
       const trackData = storageTracks[trackId] || {};
       resolve(trackData);
@@ -12,13 +12,10 @@ export async function storageGetTrack(trackId) {
 
 export async function storageUpdateTrack(trackId, trackData) {
   return new Promise((resolve) => {
-    chrome.storage.sync.get([STORAGE_TRACKS], (storageData = {}) => {
+    chrome.storage.local.get([STORAGE_TRACKS], (storageData = {}) => {
       const storageTracks = storageData[STORAGE_TRACKS] || {};
       storageTracks[trackId] = trackData;
-      const dataToSync = {
-        [STORAGE_TRACKS]: storageTracks,
-      };
-      chrome.storage.sync.set(dataToSync, resolve);
+      chrome.storage.local.set({ [STORAGE_TRACKS]: storageTracks }, resolve);
     });
   });
 }
